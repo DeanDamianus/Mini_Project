4eb86990aa32f6a0e8be4b7acf69e52af8430082
DeanDamianus <damianus.atisanta@ti.ukdw.ac.id>
2023-04-27 03:25:26 +0700

    tidurbeneran.dart

--- RecyclerApp/lib/HomePage.dart
+++ RecyclerApp/lib/HomePage.dart
@@ -8,6 +8,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                'https://media.istockphoto.com/id/1162752096/id/vektor/ilustrasi-vektor-tanda-daur-ulang.jpg?s=612x612&w=0&k=20&c=ZqErz5KqKUeicER-S5vg1dBkwEtsAfjLmynx1-edtoQ=',
+                fit: BoxFit.contain,
+                height: 32,
+              ),
+              Container(
+                  padding: const EdgeInsets.all(8.0), child: Text('Re-Cycler'))
+            ],
+          ),
+        ),
         body: Center(
       child: SingleChildScrollView(
         child: Padding(
